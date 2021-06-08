3108fc69a943b966f8b65993b2f24c5fbddca6fd
tuhmo <78699189+tuhmo@users.noreply.github.com>
2021-06-08 22:41:24 +0600

    ataa aktaa valo app

---
+++ script.js
@@ -0,0 +1,9 @@
+let fname = document.querySelector("#fname")
+let lname = document.querySelector("#lname")
+let submit = document.querySelector("#btn")
+
+submit.addEventListener('click', ()=>{
+    document.write("fuck you " + fname.value +" "+ lname.value )
+})
+
+
